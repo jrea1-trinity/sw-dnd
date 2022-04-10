b01f0a5469e4c3e6c37a6b4ccebcda785de068ba
Joshua Rea <jrea1@trinity.edu>
2022-04-10 14:40:03 -0500

    theoretically changed some text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Changing the text?
         </a>
       </header>
     </div>
