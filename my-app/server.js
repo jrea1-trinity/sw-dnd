cb792a262297a049b7ae062f5a03ea7a4de5d767
QueenArthur <akc3399@gmail.com>
2022-04-13 19:34:50 -0500

    It seems that I connected express and NodeJS

---
+++ my-app/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express'); //Line 1
+const app = express(); //Line 2
+const port = process.env.PORT || 5000; //Line 3
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+
+// create a GET route
+app.get('/express_backend', (req, res) => { //Line 9
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+}); //Line 11
